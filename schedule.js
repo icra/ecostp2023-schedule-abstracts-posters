5f92cdf1fc5358e6539cf0a7966e69052d366f28
Lluís Bosch <holalluis@gmail.com>
2023-05-26 15:14:49 +0200

    first WIP

---
+++ schedule.js
@@ -0,0 +1,109 @@
+T7. Algal systems for WW treatment and RR (14:15-16:00)
+Chairs: Ramon Barat (Univ. Polytecnic Valencia) & Rosario Rodero (Univ. Valladolid)
+  7.1. Microalgae Biorefinery for Sustainable Recovery of Bioproducts and Bioenergy from Wastewater.
+  Evelyn Ruales Dávila. Univ.Politècnica de Catalunya
+  7.2. Pilot Scale Wastewater Remediation Using Algae Bacterial Aggregated Flocs (ABAF).
+  Andrew Ward. The Univ. of Queensland
+  7.3. Anaerobic and microalgae-based treatments: potential for virus inactivation during secondary treatment of municipal wastewater.
+  Andres Torres-Franco. Univ. of Valladolid
+  7.4. The Effect of Light Cycling in the Formation of Algae-Bacteria Aggregated Flocs.
+  Holly Stolberg.  The Univ. of Queensland.
+  7.5. Comparison of High Rate Algal Pond mesocosm performance using filamentous algae or microalgae.
+  Rupert Craggs. Nat. Inst. Water and Atmospheric Research NZ.
+  7.6. Wastewater grown microalgae as biofertilizer: Contaminants of Emerging Concern, heavy metals and pathogens assessment.
+  Ana Álvarez González.  Univ. Politècnica de Catalunya.
+  7.7. Effect of veterinary antibiocs and heavy metals in the composition and valorization of a consortium of microalgae and bacteria.
+  Elena M.  Rojo. Univ. of Valladolid
+  7.8. Valorisation of microalgae grown in food waste digestate as biofertilizer.
+  Ana Álvarez González.  Univ. Politècnica de Catalunya.
+
+T10. Membranes (14:15-16:00)
+Chairs: Watsa Khongnakorn (Prince of Songkla Univ.) & Vicky Ruano (Univ.  Valencia)
+  10.1. Produced water treatment by membrane aerated biofilm reactors at elevated oxygen partial pressures.
+  Borja Valverde. DTU
+  10.2. Biological Processes Modelling for Integrated MBR Systems: A Review of the State-of-the-Art.
+  Giorgio Mannina.  Palermo University.
+  10.3. Modelling the impacts of operational conditions on the performance of a fullscale membrane aerated biofilm reactor.
+  Xavier Flores-Alsina, DTU.
+  10.4. (short presentation, 14:37-14:41))
+  Granular Anaerobic Membrane Bioreactor for low-energy domestic wastewater treatment.
+  Lucie Sanchez. Univ. de Montpellier
+  10.5. (short presentation, 14:42- 14:46) 
+  Low temperature anaerobic membrane bioreactor (AnMBR) demonstrator plant: effects of influent characterisation and site operation.
+  Matthew Palmer, Severn Trent
+  10.6. (short presentation, 14:47-14:51)
+  New framework for standardized notation in membrane filtration modelling for resource recovery from municipal wastewater.
+  Valeria Sandoval. Univ. de València
+  10.7. (short presentation, 14:52-14:56)
+  Recycled membranes for treating urban wastewater using gravity-driven force.
+  Bianca Zappulla. LEQUIA-UdG
+  - Questions/discussion (14:57-15:10)
+  - IWA SG MBR MODELLING (15:11-15:30)
+====================================================
+
+T4. WW treament for Water reclamation (16:30-18:15)
+Chairs: Wolfgang Gernjak (ICRA) & Javier Marugan (URJC)
+  16:30- 16:45 4.1. Comparing Efficiency in Solar Water Treatment: Photovoltaic-LED vs. Compound Parabolic Collector Photoreactors.
+  MARIA DOLORES MOLINA RAMIREZ. Univ. Rey Juan Carlos
+  16:45-17:00 4.2. Peroxymonosulfate/Solar process for the simultaneous disinfection and decontamination of urban wastewater at pilot plant scale.
+  Ilaria Berruti, CIEMAT-PSA
+  17:00-17:15 4.3. Chlorine-free inactivation of E. coli in water with manganese oxide-doped graphene-based electrodes.
+  Anna Segués, ICRA
+  17:15-17:30 4.4. LIFE RECYCLO: Recycling wastewater from small and medium sized laundries with advanced oxidation process.
+  Baptiste Mathon, Treewater
+  17:30-17:45 4.5. Innovative Dual Membrane System for Integrated Water-energy Recovery from Municipal Wastewater.
+  Conghui He. Tsinghua Univ.
+  17:45-17:50 4.6. Application of UV-B and UV-C light-emitting diodes (LEDs) for the removal of diclofenac in drinking water.
+  Cristina Pablos Carro. Univ. Rey Juan Carlos
+  17:50-17:55 4.7. Natural based solutions combined with solar processes at pilot scale for urban wastewater reclamation.
+  Alba Hernández Zanoletty. PSA-CIEMAT
+  17:55-18:00 4.8. Assessment of the Integration of a Vermifilter and a Zooplankton-Based Reactor for the Removal of Microcontaminants to Produce Reusable Water.
+  Manuela Hidalgo. Univ. Girona
+  18:00-18:15 Questions/discussion
+  18:15- 18:45 Poster session
+
+Workshop I. Sewer Epidemiology (16:30-18:15)
+Chairs: Laura Guerrero Latorre (ICRA) & Jorge Rodriguez (Khalifa University)
+  Zooming in to the neighbourhood level: a year-long
+  wastewater-based epidemiology SARS-CoV-2
+  monitoring campaign.
+  Ian Zammit. ICRA
+
+  Development of a method to detect recent human
+  adenovirus F41 variants in wastewater: Is it linked
+  to the new acute hepatitis?
+  Zeynep Cetecioglu, KTH
+
+  SARS-CoV-2 surveillance in the wastewater of
+  Stockholm and Malmö: the Swedish perspective.
+  Mariel Perez-Zabaleta, KTH
+
+  Surveillance of SARS-CoV-2 in sewage from
+  buildings housing residents with different
+  vulnerability levels.
+  Anna Pico, ICRA
+
+  Questions/ Discussion
+
+Workshop II. Urban Hydrosocial Cycle: Why should engineers care? (16:30-18:15)
+Chairs: Alexandra Popartan (LEQUIA-UdG) & Josep Pueyo (ICRA)
+  Assessment of flood vulnerability through
+  a multidimensional index.
+  Ana Noemi Gomez Vaca. Univ. Girona
+
+  Eco-cultural technologies for rural and
+  Maori community on-site wastewater
+  treatment in New Zealand.
+  Rupert Craggs, Nat. Inst. Water and Atmospheric Research NZ
+
+  Socio-economic criteria for preventing and controlling phosphorus pollution from municipal wastewater effluents.
+  Edgar Martin Hernandez. Univ. Laval
+
+  A hydrosocial approach to domestic water users satisfaction through Agent-Based Modelling.
+  Pol Vidal Lamolla. LEQUIA-UdG
+
+  Roadmap and strategic routes to migate micropollutant occurrence in surface water bodies through WWTP upgrade.
+  Morgan Abily. ICRA
+
+  Questions/discussion 
+
